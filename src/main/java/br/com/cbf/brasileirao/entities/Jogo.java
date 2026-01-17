f74bbee34fd1d5b213fec0962b7af527cd2809d6
vesrozeno <vesrozeno@gmail.com>
2026-01-16 22:33:05 -0300

    feat: adding database config

--- src/main/java/br/com/cbf/brasileirao/entities/Jogo.java
+++ src/main/java/br/com/cbf/brasileirao/entities/Jogo.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
@@ -14,8 +16,14 @@ public class Jogo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne
+    @JoinColumn(name = "time_mandante_id", nullable = false)
     private Time timeMandante;
+
+    @ManyToOne
+    @JoinColumn(name = "time_visitante_id", nullable = false)
     private Time timeVisitante;
+
     private Integer golsTimeMandante;
     private Integer golsTimeVisitante;
 }
--- src/main/java/br/com/cbf/brasileirao/entities/Time.java
+++ src/main/java/br/com/cbf/brasileirao/entities/Time.java
@@ -1,5 +1,6 @@
 package br.com.cbf.brasileirao.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +15,10 @@ public class Time {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(length = 20)
     private String nome;
+    @Column(length = 3)
     private String sigla;
+    @Column(length = 2)
     private String uf;
 }
